61927277337615e4c909e77d2bae68dc5902d01d
GusEmery <nfemery@outlook.com>
2022-09-09 18:56:36 +0000

    Adding Build Directory

---
+++ build/process-codecoverage.ps1
@@ -0,0 +1,8 @@
+﻿$files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
+Write-Host $env:USERPROFILE
+foreach ($file in $files)
+{
+    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\16.11.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    Write-Host $command
+    Invoke-Expression $command
+}
